2fc6cc9ebd5f153613eaa5da87d0cd4ec2603071
wizard-technology <dv.hamakamaran@gmail.com>
2021-01-04 02:31:07 +0300

    first commit

---
+++ app/Application.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Application extends Model
+{
+    public function channel()
+    {
+        return $this->hasMany(ChannelApp::class,'ac_app');
+    }
+    
+}
